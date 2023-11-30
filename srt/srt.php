ce43cc1969834919bd1f9a347df66d47b412de49
vincseize <vincseize@gmail.com>
2023-11-30 01:06:44 +0100

    Update srt.php

--- srt/srt.php
+++ srt/srt.php
@@ -1,106 +1,75 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <?php
-$file = 
-include_once 'srtReader.php';
-$file = './assets/models/camilia/camilia.srt';
-$jsonData = srtReader($file);
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+define('SRT_STATE_SUBNUMBER', 0);
+define('SRT_STATE_TIME',      1);
+define('SRT_STATE_TEXT',      2);
+define('SRT_STATE_BLANK',     3);
+
+function srtReader($srt) {
+    $lines = file($srt);
+    $subs = array();
+    $state = SRT_STATE_SUBNUMBER;
+    $subNum = 0;
+    $subText = '';
+    $subTime = '';
+
+    foreach ($lines as $line) {
+        switch ($state) {
+            case SRT_STATE_SUBNUMBER:
+                $subNum = trim($line);
+                $state  = SRT_STATE_TIME;
+                break;
+
+            case SRT_STATE_TIME:
+                $subTime = trim($line);
+                $state   = SRT_STATE_TEXT;
+                break;
+
+            case SRT_STATE_TEXT:
+                if (trim($line) == '') {
+                    // Check if a subtitle with the same number already exists
+                    $existingSub = end($subs);
+                    if ($existingSub && $existingSub->number == $subNum) {
+                        // If it exists, append the text to the existing subtitle
+                        $existingSub->text .= $subText;
+                    } else {
+                        // If not, create a new subtitle entry
+                        $sub = new stdClass;
+                        $sub->number = $subNum;
+                        list($sub->startTime, $sub->stopTime) = explode(' --> ', $subTime);
+                        $sub->text = $subText;
+                        $subs[] = $sub;
+                    }
+
+                    $subText = '';
+                    $state = SRT_STATE_SUBNUMBER;
+                } else {
+                    // Append the line to the text
+                    $subText .= $line;
+                }
+                break;
+        }
+    }
+
+    // Check if there's an unfinished subtitle after the loop
+    if (!empty($subNum) && !empty($subTime) && !empty($subText)) {
+        $sub = new stdClass;
+        $sub->number = $subNum;
+        list($sub->startTime, $sub->stopTime) = explode(' --> ', $subTime);
+        $sub->text = $subText;
+        $subs[] = $sub;
+    }
+
+    // Convert PHP array to JSON format
+    return json_encode($subs, JSON_PRETTY_PRINT);
+}
+
+// Usage example
+// $srtFilePath = './path/to/your/file.srt';
+// $jsonData = srtReader($srtFilePath);
 // echo $jsonData;
-
 ?>
-
-<!-- Use the JSON data in JavaScript -->
-<script>
-const srtContentPHP = <?php echo $jsonData; ?>;
-// console.log(srtContentPHP);
-</script>
-
-
-
-
-<head>
-<meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Subtitle Loader</title>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <!-- <script src="./js/srt-parser.js"></script> -->
-
-  <script src="./js/timer.js"></script>
-
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-      margin: 0;
-      padding: 0;
-      background-color: #f4f4f4;
-    }
-
-    header {
-      background-color: #333;
-      color: #fff;
-      padding: 10px;
-      text-align: center;
-    }
-
-    main {
-      max-width: 800px;
-      margin: 20px auto;
-      padding: 20px;
-      background-color: #fff;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    }
-
-    footer {
-      text-align: center;
-      padding: 10px;
-      background-color: #333;
-      color: #fff;
-    }
-
-    .subtitle-container {
-      text-align: left;
-      background-color: #333;
-      color: #fff;
-      /* border: solid 1px gray; */
-    }
-
-  </style>
-
-</head>
-<body>
-
-  <header>
-    <h1>Subtitle Loader</h1>
-  </header>
-  
-  <div id="timer">00:00:00,000</div>
-
-  <div id="subtitle-container" class="subtitle-container"></div>
-
-
-<script>
-
-    const srtContent = <?php echo $jsonData; ?>;
-    console.log(srtContent);
-
-    // Get the timer element
-    const timerElement = document.getElementById('timer');
-
-    // Set times in seconds (e.g., 5 seconds)
-    const startTime = '00:00:00,000';
-    const endTime='00:00:05,010';
-    //
-
-    initTimer(startTime, endTime);
-
-</script>
-
-
-
-
-
-
-
-</body>
-</html>
